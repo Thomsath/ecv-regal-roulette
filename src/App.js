4869f65185749315c21ed5f993042c937a119716
Barruchou <felix.barruche@laposte.net>
2019-09-10 13:47:55 +0200

    add mains routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import RouteHome from "./components/RouteHome";
+import RouteEdit from "./components/RouteEdit";
+import RouteWheel from "./components/RouteWheel";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Route exact path="/" component={RouteHome} />
+          <Route exact path="/edit" component={RouteEdit} />
+          <Route exact path="/wheel" component={RouteWheel} />
+      </Router>
   );
 }
 
---
+++ src/components/RouteEdit.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class RouteEdit extends React.Component {
+    render() {
+        return <h1>Edit.</h1>;
+    }
+}
+
+export default RouteEdit;
